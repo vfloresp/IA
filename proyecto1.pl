4b316264c59758f36857ec3fe9fcc86706a1e409
Victor desktop <vflorespineda@gmail.com>
2019-02-04 20:23:48 -0600

    suma y resta

--- proyecto1.pl
+++ proyecto1.pl
@@ -13,10 +13,41 @@ comparaLong(ListaA,ListaB):-
     L2 is Y,
     L1 =:= L2.
 
-suma([[]|A],[[]|B],[_|C]):-
-    C is A + B.
+sumaLista([],[],[]).
+sumaLista([A|ColaA],[B|ColaB],[C|ColaC]):-
+         comparaLong(ColaA,ColaB),
+         sumaLista(ColaA,ColaB,ColaC),
+         C is A + B.
+
+suma([],[],[]).
 suma([A|ColaA],[B|ColaB],[C|ColaC]):-
-    comparaLong(A,B),
-    suma(A,B,C),
+    comparaLong(ColaA,ColaB),
+    sumaLista(A,B,C),
     suma(ColaA,ColaB,ColaC).
+
+restaLista([],[],[]).
+restaLista([A|ColaA],[B|ColaB],[C|ColaC]):-
+         comparaLong(ColaA,ColaB),
+         restaLista(ColaA,ColaB,ColaC),
+         C is A - B.
+
+resta([],[],[]).
+resta([A|ColaA],[B|ColaB],[C|ColaC]):-
+    comparaLong(ColaA,ColaB),
+    restaLista(A,B,C),
+    resta(ColaA,ColaB,ColaC).
+
+listaXelem([],[]).
+listaXelem([A|ColaA],[B|ColaB]):-
+         transpone(ColaA,ColaB),
+         B = [A].
+
+rellenaLista(_,[]).
+rellenaLista([A|ColaA],[B|ColaB]):-
+         A = [A,B]
+         .
+
+transpone([A|ColaA],B):-
+         listaXelem(A,B),
+         rellenaLista(B,ColaA).
 
