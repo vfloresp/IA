de5a88b0605656c410dfb12d3033de37c03f0c4a
Victor <vflorespineda@gmail.com>
2019-03-13 17:31:08 -0600

    Trabajando en el generador de arbol

--- domino/AlphaBeta.pl
+++ domino/AlphaBeta.pl
@@ -1,22 +1,22 @@
-%Base de datos con la notación de [[6,6], costo]
+%Base de datos con la notaciï¿½n de [[6,6], costo]
 
-listaAbierta([X,Y]).
-listaFichas([[6,6],_],[[5,6],_],[[4,6],_],[[3,6],_],[[2,6],_],[[1,6],_],[[0,6],_],[[5,5],_],[[4,5],_],[[3,5],_],[[2,5],_],[[1,5],_],[[0,5],_],[[4,4],_],[[3,4],_],[[2,4],_],[[1,4],_],[[0,4],_],[[3,3],_],[[2,3],_],[[1,3],_],[[0,3],_],[[2,2],_],[[1,2],_],[[0,2],_],[[1,1],_],[[0,1],_],[[0,0],_]).
-listaEnMano().
-listaEnMesa().
-NumComer(14).
-miMano(N).
-manoOponente(M).
+% listaAbierta([X,Y]).
+% listaFichas([[6,6],_],[[5,6],_],[[4,6],_],[[3,6],_],[[2,6],_],[[1,6],_],[[0,6],_],[[5,5],_],[[4,5],_],[[3,5],_],[[2,5],_],[[1,5],_],[[0,5],_],[[4,4],_],[[3,4],_],[[2,4],_],[[1,4],_],[[0,4],_],[[3,3],_],[[2,3],_],[[1,3],_],[[0,3],_],[[2,2],_],[[1,2],_],[[0,2],_],[[1,1],_],[[0,1],_],[[0,0],_]).
+% listaEnMano().
+% listaEnMesa().
+% NumComer(14).
+% miMano(N).
+% manoOponente(M).
 
-listaIncognitas([],_,_,_):-
-    !.
-listaIncognitas(ListaFichas,ListaEnMano,ListaEnMesa,ListaIncognitos):-
-    ListaFichas = [CabezaF|ColaF],
-    (member(CabezaF,ListaEnMano) == false ->
-    append(CabezaF,ListaIncognitos,ListaIncognitos)),
-    (member(CabezaF,ListaEnMesa)== false ->
-    append(CabezaF,ListaIncognitos,ListaIncognitos)),
-    listaIncognitas(ColaF,ListaEnMano,ListaEnMesa,ListaIncognitos).
+% listaIncognitas([],_,_,_):-
+%     !.
+% listaIncognitas(ListaFichas,ListaEnMano,ListaEnMesa,ListaIncognitos):-
+%     ListaFichas = [CabezaF|ColaF],
+%     (member(CabezaF,ListaEnMano) == false ->
+%     append(CabezaF,ListaIncognitos,ListaIncognitos)),
+%     (member(CabezaF,ListaEnMesa)== false ->
+%     append(CabezaF,ListaIncognitos,ListaIncognitos)),
+%     listaIncognitas(ColaF,ListaEnMano,ListaEnMesa,ListaIncognitos).
 
 %EjemploArbol is [[6,6],[[6,5],[6,4]]].%
 %BaseDatos is [[6,6],[5,6],[4,6],[3,6],[2,6],[1,6],[0,6],[5,5],[4,5],[3,5],[2,5],[1,5],[0,5],[4,4],[3,4],[2,4],[1,4],[0,4],[3,3],[2,3],[1,3],[0,3],[2,2],[1,2],[0,2],[1,1],[0,1],[0,0]].%
@@ -104,31 +104,21 @@ childrenMin(Children,Value,Depth,Alpha,Beta,ValueRet):-
 tomaUna(_,0,Player):-
     Player is 0.
 
-tomaUna(ListaEnMano,NumComer,Player):-
-    read([[X,Y],Costo]),nl,
-    append([[X,Y],Costo],ListaEnMano,ListaEnMano),
-    b_set(NumComer,NumComer-1).
-
 buscaAbierto([],_,_,_,_):-
     !.
-buscaAbierto([CabezaLM|ColaLM],[X|Y],ListaJugada,ListaEnMano,NumComer,Player):-
-    CabezaLM = [NumFicha|Peso],
+buscaAbierto([Cabeza|Cola],[X|Y],Hijos):-
+    Cabeza = [NumFicha|Peso],
     (member(X,NumFicha) ->
      NumFicha = [Num1|Num2],
      (Num1=:=Num2 -> Peso is Num1+Num2+13),
      (Num1=\=Num2 -> Peso is Num1+Num2),
-     append(ListaJugada,CabezaLM,ListaJugada)),
-    (((member(X,NumFicha),member(Y,NumFicha)) == false), member(Y,NumFicha) ->
+     append(Hijos,Cabeza,Hijos)),
+    %(((member(X,NumFicha),member(Y,NumFicha)) == false), member(Y,NumFicha) ->
+    (member(Y,NumFicha) ->
      NumFicha = [Num1|Num2], Peso is Num1+Num2,
-     append(ListaJugada,CabezaLM,ListaJugada)),
-
+     append(Hijos,Cabeza,Hijos)),
     ((member(X,NumFicha) == false);(member(Y,NumFicha) == false);((member(X,NumFicha),member(Y,NumFicha)) == false) ->
-    tomaUna(ListaEnMano,NumComer,Player)),
-
-    buscaAbierto(ColaLM,[X|Y],ListaJugada,ListaEnMano,NumComer,Player).
-
-funcionYasAsi(ListaJugada,ListaAbietos,ListaJugada,ListaEnMano,NumComer,Depth,Alpha,Beta,Player,ValorH):-
-    buscaAbierto(ListaJugada,ListaAbietos,ListaJugada,ListaEnMano,NumComer,Player),
-    alphaBeta(ListaJugada,Depth,Alpha,Beta,Player,ValorH).
+    write("Tienes que comer")),
+    buscaAbierto(Cola,[X|Y],Hijos).
 
 
